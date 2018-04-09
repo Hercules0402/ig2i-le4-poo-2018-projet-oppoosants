ce660f1540176e8962e31e9ab1e081b046dbe7f9
Sébastien CORNUEL <sbastien.cornuel@gmail.com>
2018-04-09 16:32:35 +0200

    Update Reader.java

--- ProjetPOO/src/util/Reader.java
+++ ProjetPOO/src/util/Reader.java
@@ -1,6 +1,7 @@
 package util;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
@@ -21,11 +22,11 @@ public class Reader {
 	 */
 	public Reader(String filename) throws Exception {
 		if (filename == null) {
-			System.err.println("Erreur : Impossible d'ouvrir le fichier ...");
-			System.err.println("=> Vérifier que le fichier existe bien dans le dossier.");
+			System.err.println("Une erreur a été rencontrée : Impossible d'ouvrir le fichier ...");
 			throw new Exception();
 		}
 		this.instanceFile = new File(filename + ".txt");
+        System.out.println("Fichier trouvé.");
 		
 	}
 	
@@ -34,10 +35,19 @@ public class Reader {
 	 * @param scan TODO
 	 * @return int
 	 */
-	private int lireProchainEntier(Scanner scan) {
+	private int readNextInt(Scanner scan) {
 		while (scan.hasNext("//.*")) {
 			scan.nextLine();
 		}
 		return Integer.parseInt(scan.next());
 	}
+    
+    public int getNbLocations() throws FileNotFoundException {
+        return readNextInt(new Scanner(this.instanceFile));
+    }
+    
+    public static void main(String[] args) throws Exception {
+        Reader r = new Reader("instance_0116_131940_Z2");
+        System.out.println(r.getNbLocations());
+    }
 }
