a280a9542fb4e34e52deb52e3f1625202ea68831
Sébastien CORNUEL <sbastien.cornuel@gmail.com>
2018-04-09 16:46:33 +0200

    Update Reader.java

--- ProjetPOO/src/util/Reader.java
+++ ProjetPOO/src/util/Reader.java
@@ -29,7 +29,11 @@ public class Reader {
         System.out.println("Fichier trouvé.");
         
     }
-    
+
+    public File getInstanceFile() {
+        return instanceFile;
+    }
+
     /**
      * Permet de récupérer les différents paramètres de type int comme NbLocations.
      * @param scan TODO
@@ -41,13 +45,39 @@ public class Reader {
         }
         return Integer.parseInt(scan.next());
     }
-    
-    public int getNbLocations() throws FileNotFoundException {
-        return readNextInt(new Scanner(this.instanceFile));
+
+    /**
+     * Retourne le nombre de localisations.
+     * @param scan TODO
+     * @return int
+     */
+    public int getNbLocations(Scanner scan) {
+        return readNextInt(scan);
     }
-    
+
+    /**
+     * Retourne le nombre de produits.
+     * @param scan TODO
+     * @return int
+     */
+    public int getNbProducts(Scanner scan) {
+        return readNextInt(scan);
+    }
+
+    /**
+     * Main.
+     * @param args TODO
+     * @throws Exception 
+     */
     public static void main(String[] args) throws Exception {
         Reader r = new Reader("instance_0116_131940_Z2");
-        System.out.println(r.getNbLocations());
+        Scanner scan = null;
+        try {
+            scan = new Scanner(r.getInstanceFile());
+            System.out.println("NbLocations : " + r.getNbLocations(scan));
+            System.out.println("NbProducts : " + r.getNbProducts(scan));
+        } catch (FileNotFoundException ex) {
+            System.err.println("Une erreur a été rencontrée : Fichier introuvable...");
+        }
     }
 }
